be1693d2c73b184a092de2377ea0dcf2520b0d90
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-01-08 15:48:59 +0530

    add function to format time

--- client/components/ChatWindow.jsx
+++ client/components/ChatWindow.jsx
@@ -4,6 +4,33 @@ const ChatWindow = ({ username, roomId, socket }) => {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [activityMsg, setActivityMsg] = useState("");
-  return <div>  </div>;
+  return (
+    <div>
+      <div>
+        <h2>Room: {roomId}</h2>
+        <p>
+          Welcom, <span>{username}</span>
+        </p>
+      </div>
+      <div>
+        {messages.map((message) => {
+          const { id, text, type, userName } = message || {};
+          if (type === "notif") {
+            return (
+              <div key={id} style={{ color: "green" }}>
+                {text}
+              </div>
+            );
+          }else{
+            return(
+                <div key={id}>
+                    
+                </div>
+            )
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 export default ChatWindow;
